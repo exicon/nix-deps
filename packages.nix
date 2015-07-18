f940afa0d49aa5b354f28e846416cabed6c7a4f0
Stewart Mackenzie <setori88@gmail.com>
2015-07-18 14:08:00 +0800

    added boot

--- packages.nix
+++ packages.nix
@@ -30,4 +30,30 @@ with import <nixpkgs> {};
       platforms = stdenv.lib.platforms.all;
     };
   };
+
+  boot = stdenv.mkDerivation rec {
+    version = "2.0.0";
+    name = "boot-${version}";
+
+    src = fetchurl {
+      url = "https://github.com/boot-clj/boot/releases/download/${version}/boot.sh";
+      sha256 = "12c24aqvwq8kj6iiac18rp0n8vlzacl7dd95m983yz24w885chc0";
+    };
+
+    inherit jdk;
+
+    builder = ./boot_builder.sh;
+
+    buildInputs = [ makeWrapper ];
+
+    propagatedBuildInputs = [ jdk ];
+
+    meta = {
+      description = "Build tooling for Clojure";
+      homepage = http://boot-clj.com/;
+      license = stdenv.lib.licenses.epl10;
+      platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
+      maintainers = [ stdenv.lib.maintainers.sjmackenzie ];
+    };
+  };
 }
