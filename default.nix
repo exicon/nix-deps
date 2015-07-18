494fd305e4efe797a073d998411669181517583a
Stewart Mackenzie <setori88@gmail.com>
2015-07-18 16:22:05 +0800

    mv packages.nix->default.nix to ease appboard dep calls

--- default.nix
+++ default.nix
@@ -1,9 +1,60 @@
-{ nixpkgs ? <nixpkgs>
-, system ? builtins.currentSystem
-}:
+with import <nixpkgs> {};
+{
+  zerotierone = stdenv.mkDerivation rec {
+    version = "1.0.2";
+    name = "zerotierone-${version}";
 
-let
-  pkgs = import nixpkgs {};
+    src = fetchurl {
+      url = "https://github.com/zerotier/ZeroTierOne/archive/${version}.tar.gz";
+      sha256 = "002ay4f6l9h79j1708klwjvinsv6asv24a0hql85jq27587sv6mq";
+    };
 
-  jobs = imp
+    preConfigure = ''
+      substituteInPlace ./make-linux.mk \
+          --replace 'CC=$(shell which clang gcc cc 2>/dev/null | head -n 1)' "CC=${gcc}/bin/gcc";
+      substituteInPlace ./make-linux.mk \
+          --replace 'CXX=$(shell which clang++ g++ c++ 2>/dev/null | head -n 1)' "CC=${gcc}/bin/g++";
+    '';
 
+    buildInputs = [ openssl lzo zlib gcc ];
+
+    installPhase = ''
+      installBin zerotier-one
+    '';
+
+    meta = {
+      description = "Create flat virtual Ethernet networks of almost unlimited size";
+      homepage = https://www.zerotier.com;
+      license = stdenv.lib.licenses.gpl3;
+      maintainers = [ stdenv.lib.maintainers.sjmackenzie ];
+      platforms = stdenv.lib.platforms.all;
+    };
+  };
+
+  boot = stdenv.mkDerivation rec {
+    version = "2.0.0";
+    name = "boot-${version}";
+
+    src = fetchurl {
+      url = "https://github.com/boot-clj/boot/releases/download/${version}/boot.sh";
+#      sha256 = "12c24aqvwq8kj6iiac18rp0n8vlzacl7dd95m983yz24w885chc0";
+      sha256 = "0hvznr0z5ck32wpaqhjv2fr9365b8h0h8brnsmwijpv699l0rf1c";
+    };
+
+    inherit jdk;
+
+    builder = ./boot_builder.sh;
+
+    buildInputs = [ makeWrapper ];
+
+    propagatedBuildInputs = [ jdk ];
+
+    meta = {
+      description = "Build tooling for Clojure";
+      homepage = http://boot-clj.com/;
+      license = stdenv.lib.licenses.epl10;
+      platforms = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
+      maintainers = [ stdenv.lib.maintainers.sjmackenzie ];
+    };
+  };
+}
